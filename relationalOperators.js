6a81410b7b201f2fe38579e5901b71feeb479b7c
shubamWanclouds <sdadhwal@wanclouds.net>
2021-08-06 20:44:02 +0530

    basic changes

--- relationalOperators.js
+++ relationalOperators.js
@@ -1,39 +1,55 @@
 function check() {
   let num1 = parseInt(document.getElementById("num1").value);
   let num2 = parseInt(document.getElementById("num2").value);
-  let operator = document.getElementById("operator").value;
-  switch (operator) {
-    case "==":
-      alert (num1 == num2);
-      break;
-    case "===":
-      alert (num1 === num2);
-      break;
-    case "!=":
-      alert (num1 != num2);
-      break;
-    case ">":
-      alert (num1 > num2);
-      break;
-    case ">=":
-      alert (num1 >= num2);
-      break;
-    case "+":
-      alert (num1 + num2);
-      break;
-    case "-":
-      alert (num1 - num2);
-      break;
-    case "*":
-      alert (num1 * num2);
-      break;
-    case "/":
-      alert (num1 / num2);
-      break;
-    case "%":
-      alert (num1 % num2);
-      break;
-    default:
-      alert("something wrong happened");
-  }
+  document.getElementById("results").innerHTML += "num1 + num2 :  " + (num1 + num2);
+  document.getElementById("results").innerHTML += "<br> num1 - num2 :  " + (num1 - num2);
+  document.getElementById("results").innerHTML += "<br> num1 * num2 :  " + (num1 * num2);
+  document.getElementById("results").innerHTML += "<br> num1 / num2 :  " + (num1 / num2);
+  document.getElementById("results").innerHTML += "<br> num1 % num2 :  " + (num1 % num2);
+  document.getElementById("results").innerHTML += "<br> num1 > num2 :  " + (num1 > num2);
+  document.getElementById("results").innerHTML += "<br> num1 >= num2 :  " + (num1 >= num2);
+  document.getElementById("results").innerHTML += "<br> num1 < num2 :  " + (num1 < num2);
+  document.getElementById("results").innerHTML += "<br> num1 <= num2 :  " + (num1 <= num2);
+  document.getElementById("results").innerHTML += "<br> num1 == num2 :  " + (num1 == num2);
+  document.getElementById("results").innerHTML += "<br> num1 != num2 :  " + (num1 != num2);
+//   switch (operator) {
+//     case "==":
+//       alert (num1 == num2);
+//       break;
+//     case "===":
+//       alert (num1 === num2);
+//       break;
+//     case "!=":
+//       alert (num1 != num2);
+//       break;
+//     case ">":
+//       alert (num1 > num2);
+//       break;
+//     case ">=":
+//       alert (num1 >= num2);
+//       break;
+//     case "<":
+//       alert (num1 < num2);
+//       break;
+//     case "<=":
+//       alert (num1 <= num2);
+//       break;
+//     case "+":
+//       alert (num1 + num2);
+//       break;
+//     case "-":
+//       alert (num1 - num2);
+//       break;
+//     case "*":
+//       alert (num1 * num2);
+//       break;
+//     case "/":
+//       alert (num1 / num2);
+//       break;
+//     case "%":
+//       alert (num1 % num2);
+//       break;
+//     default:
+//       alert("something wrong happened");
+//   }
 }
