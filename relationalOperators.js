635485d649f4cb0b2a2bb68fa15501a8fe92c373
shubamWanclouds <sdadhwal@wanclouds.net>
2021-08-06 14:37:02 +0530

    switch styling improved

--- relationalOperators.js
+++ relationalOperators.js
@@ -3,15 +3,22 @@ function check() {
   let num2 = document.getElementById("num2").value;
   let operator = document.getElementById("operator").value;
   switch (operator) {
-    case "==": alert (num1 == num2);
-               break;
-    case "===": alert (num1 === num2);
-                break;
-    case "!=": alert (num1 != num2);
-               break;
-    case ">": alert (num1 > num2);
-              break;
-    case ">=": alert (num1 >= num2);
-               break;
+    case "==":
+      alert (num1 == num2);
+      break;
+    case "===":
+      alert (num1 === num2);
+      break;
+    case "!=":
+      alert (num1 != num2);
+      break;
+    case ">":
+      alert (num1 > num2);
+      break;
+    case ">=":
+      alert (num1 >= num2);
+      break;
+    default:
+      alert("something wrong happened");
   }
 }
