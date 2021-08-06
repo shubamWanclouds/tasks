bfde75da71f970d9df51d9d54cb6d302b2d48502
shubamWanclouds <sdadhwal@wanclouds.net>
2021-08-06 13:59:58 +0530

    optimized code

--- palindrome.js
+++ palindrome.js
@@ -1,7 +1,7 @@
 function palindome() {
     let word = document.getElementById("word").value;
     const length = word.length;
-    for (let i=0; i<length/2; i++) {
+    for (let i = 0; i < length/2; i++) {
         if (word[i] != word[length-1-i]) {
           alert("Not a Palindrome!");
           return;
